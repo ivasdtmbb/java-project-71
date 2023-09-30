b3ce7d8b6c8cc263b903ddbeabe1d8ccd456a826
ivasdtmbb <ivasdtmbb@gmail.com>
2023-09-30 16:30:19 +0800

    Add a picocli console utility

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true,
+            description = "Compares two configuration files and shows a difference.")
+    class App implements Runnable {
+
+        @Option(names = {"-h", "--help"}, description = "Show this @|blue help|@ message and exit.")
+        private String help;
+//        @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+//        private String version;
+
+        @Override
+        public void run() {
+            System.out.println("Hello World!");
+        }
+
+        public static void main(String[] args) {
+            System.exit(new CommandLine(new App()).execute(args));
+
+        }
     }
-}
