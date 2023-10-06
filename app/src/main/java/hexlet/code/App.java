f3adf194c9a6518dc84a48bc450af9135e090714
ivasdtmbb <ivasdtmbb@gmail.com>
2023-10-06 15:40:42 +0800

    Modify App.java
    Add --format option to picocli
    Add two filepath arguments to picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,14 +5,30 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true,
             description = "Compares two configuration files and shows a difference.")
     class App implements Runnable {
 
-        @Option(names = {"-h", "--help"}, description = "Show this @|blue help|@ message and exit.")
-        private String help;
-//        @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-//        private String version;
+        @Option(names = {"-h", "--help"}, usageHelp = true,
+                description = "Show this @|blue help|@ message and exit.")
+        private boolean helpRequested = false;
+
+        @Option(names = { "-V", "--version" }, versionHelp = true,
+                description = "Print version information and exit.")
+        boolean versionRequested;
+
+        @Option(names = { "-f", "--format=format" },
+                defaultValue = "stylish",
+                description = "output format [default: stylish]")
+        String format;
+
+        @Parameters(index = "0", description = "path to first file")
+        private File filepath1;
+
+        @Parameters(index = "1", description = "path to second file")
+        private File filepath2;
 
         @Override
         public void run() {
